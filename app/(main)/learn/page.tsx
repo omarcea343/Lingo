c58e1f06d17840a7b03578d7c8f5a127d29fafd1
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-03-17 13:36:29 -0600

    Loader

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,20 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
-import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
 
-const LearnPage = () => {
+import { Header } from "./header";
+import { redirect } from "next/navigation";
+
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
